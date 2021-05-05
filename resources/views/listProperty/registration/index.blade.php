43c41354854b5d11f668180f8a60728fa5cb1e80
lucks <lucksisack2@gmail.com>
2021-05-05 21:33:51 +0300

    progress

--- resources/views/listProperty/registration/index.blade.php
+++ resources/views/listProperty/registration/index.blade.php
@@ -3,6 +3,19 @@
 
 @section('css')
 
+    #fa-eye{
+    float: right;
+    font-size: 25px;
+    margin-top: -30px;
+    cursor: pointer;
+    }
+
+    #fa-eye i{
+
+    color: #7a7c7d;
+    }
+
+
 @stop
 
 @section('content')
@@ -16,14 +29,14 @@
                     <div class="text-center">
                         @include('partial.flash.index')
                     </div>
-                    <p id="error-input"></p>
+
                     <h4 class="text-center">Create an account to list and manage your property</h4>
 
                     <form action="{{url('list-your-property/user-registration/store')}}" method="post" id="msform">
                        @csrf
                         <div>
                             <fieldset>
-
+                                <p id="error-input"></p>
                                 <div class="form-card">
                                     <div class="col-md-12 pt-3 mb-3">
                                         <label>First Name</label><span class="star">*</span>
@@ -53,18 +66,25 @@
 
                                 <div class="form-card">
                                     <div class="pt-3">
-                                        <h6 style="color: red;" id="error-input2"></h6>
+                                        <h6 style="color: red; text-align: center;" id="error-input2"></h6>
                                     </div>
 
                                     <div class="col-md-12 mb-3">
                                         <label>Email</label><span class="star">*</span>
-                                        <input  type="email" name="email" class="form-control">
+                                        <input id="regEmail"  type="email" name="email" class="form-control">
                                     </div>
 
                                     <div class="col-md-12 mb-3">
                                         <label>Password</label><span class="star">*</span>
-                                        <input  type="password" name="password" class="form-control" >
+                                        <input id="password" type="password" name="password" class="form-control" ><span id="fa-eye"></span>
                                     </div>
+
+                                    <div class="col-md-12 mb-3">
+                                        <label>Confirm Password</label><span class="star">*</span>
+                                        <input id="passwordConfirm"  type="password" name="password2" class="form-control" >
+                                    </div>
+
+                                    <div style=" color: red;" class="col-md-12 error pb-3"></div>
 
                                 </div>
 
@@ -86,6 +106,30 @@
 
 
 @section('js')
+
+    <script>
+        $(document).ready(function (){
+
+            $('#fa-eye').html('<i class="fas fa-eye"></i>');
+
+
+            $('#fa-eye').click(function (){
+
+                if ($('#password').attr('type') == 'text') {
+                    $('#password').attr('type', 'password');
+                    $('#fa-eye').html('');
+                    $('#fa-eye').html('<i class="fas fa-eye"></i>');
+                } else {
+                    $('#fa-eye').html('<i class="fas fa-eye-slash"></i>');
+                    $('#password').attr('type', 'text');
+                }
+            });
+
+
+        });
+
+
+    </script>
 
     <script>
         $(document).ready(function(){
@@ -134,38 +178,31 @@
 
             });
 
-            $("#btn-2").click(function(){
+            $("#btn-2").click(function(e){
 
-                let phone = $("#phone").val();
-                let country = $("#country").val();
+                let email = $("#regEmail").val();
+                let password = $("#password").val();
 
 
-                if (phone != '' && country != '' )
+                if (email != '' && password != '' )
                 {
+                    let pass = $('#password').val();
 
-                    current_fs = $(this).parent();
-                    next_fs = $(this).parent().next();
+                    let passConf = $('#passwordConfirm').val();
 
-//show the next fieldset
-                    next_fs.show();
-//hide the current fieldset with style
-                    current_fs.animate({opacity: 0}, {
-                        step: function(now) {
-// for making fielset appear animation
-                            opacity = 1 - now;
+                    if (pass != passConf)
+                    {
+                        e.preventDefault();
 
-                            current_fs.css({
-                                'display': 'none',
-                                'position': 'relative'
-                            });
-                            next_fs.css({'opacity': opacity});
-                        },
-                        duration: 600
-                    });
+                        $('.error').text('Please confirm your password')
+                    }
+
                 }
 
                 else
                 {
+                    e.preventDefault();
+
                     $("#error-input2").text('Please fill in the required fields')
                 }
 
